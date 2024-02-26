db1d3be5c02836d0a3d05ca172ac84343f52f7e0
Alex Akhtyrtsev <a.akhtyrtsev@gmail.com>
2024-02-26 18:27:38 +0200

    Implement inline queries handling.

--- src/index.js
+++ src/index.js
@@ -10,21 +10,45 @@
  * Learn more at https://developers.cloudflare.com/workers/runtime-apis/scheduled-event/
  */
 
+function getStr() {
+	return '❤️'.repeat( 256 );
+}
+
 export default {
 	async scheduled(controller, env, ctx) {
-		console.log(`Hello World!`);
+		const subscribers = JSON.parse(await env.GEN.get("subscribers") || '[]')
+
+		for( const s of subscribers )
+			await this.sendMessage(env.API_KEY, s.chatId, getStr());
 	},
 	async fetch(request, env, ctx) {
 		if(request.method === "POST"){
 			const payload = await request.json();
 
+			if ( 'inline_query' in payload ) {
+				await this.answerInlineQuery(env.API_KEY, payload);
+				return new Response('OK');
+			}
+
 			if('message' in payload){
 				const chatId = payload.message.chat.id;
 				// const input = String(payload.message.text);
-				const user_firstname = String(payload.message.from.first_name);
+				// const user_firstname = String(payload.message.from.first_name);
 				// const response = user_firstname + " said " + input;
 				// await this.sendMessage(chatId, response);
-				await this.sendMessage(env.API_KEY, chatId, "Hello, " + user_firstname );
+				// await this.sendMessage(env.API_KEY, chatId, "Hello, " + user_firstname );
+
+
+				const subscribers = JSON.parse(await env.GEN.get("subscribers") || '[]')
+				if ( !subscribers.some(s => +s.chatId === +chatId) ) {
+					subscribers.push({chatId})
+					await this.sendMessage(env.API_KEY, chatId, getStr() );
+					await env.GEN.put('subscribers', JSON.stringify(subscribers))
+				}
+				else if ( payload.message.text === '/get' ) {
+					await this.sendMessage(env.API_KEY, chatId, "You have already got your Daily Hearts..." );
+					await this.sendMessage(env.API_KEY, chatId, "Anyway...\n" + getStr()  );
+				}
 			}
 		}
 		return new Response('OK');
@@ -32,5 +56,24 @@ export default {
 	async sendMessage(apiKey, chatId, text){
 		const url = `https://api.telegram.org/bot${apiKey}/sendMessage?chat_id=${chatId}&text=${text}`;
 		return await fetch(url).then(resp => resp.json());
-	}
+	},
+	async answerInlineQuery(apiKey, {inline_query: query}) {
+		const url = `https://api.telegram.org/bot${apiKey}/answerInlineQuery`;
+
+		const body = {
+			inline_query_id: query.id,
+			cache_time: 5,
+			results: JSON.stringify([
+				{
+					type: 'article',
+					id: (+new Date()).toString(16),
+					title: '❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️',
+					message_text: getStr()
+				}
+			])
+		};
+
+		return await fetch(url  + '?' + new URLSearchParams(body))
+			.then(resp => resp.json())
+	},
 };
